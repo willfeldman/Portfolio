f56e5b3859e18ae9798522ef4dee4c5697e8f212
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-07-03 13:12:00 -0400

    Get image dimensions

--- src/components/gallery/SimpleGalleryFunctions.js
+++ src/components/gallery/SimpleGalleryFunctions.js
@@ -1,9 +1,20 @@
-const { promises: fs } = require('fs');
-const dir = '/';
-
 const getNumFiles = async (dir) => {
+  const { promises: fs } = require("fs");
   const files = await fs.readdir(dir);
-  console.log(files.length);
+  return files.length;
+};
+
+async function getImageDimensions(src) {
+    const probe = require('probe-image-size');
+    let result = await probe(require('fs').createReadStream(src));
+    return result;
 }
 
-console.log(getNumFiles("src/components/gallery/gallery-images"));
+getNumFiles("src/components/gallery/gallery-images").then((num) => {
+    console.log(num);
+});
+
+getImageDimensions("src/components/gallery/gallery-images/1.jpg").then((dim) => {
+    console.log("Width: " + dim.width);
+    console.log("Height: " + dim.height);
+});
