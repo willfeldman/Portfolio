92ec37fb12318e9b899bfbea289e3583a13d8d77
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-07-03 20:21:50 -0400

    Added data

--- src/data/experience.js
+++ src/data/experience.js
@@ -6,6 +6,8 @@ var experiences = [
     description: ["Led a web performance focused 4-person scrum team in daily standups and sprint planning", "Regularly met and communicated with several internal and external stakeholders of projects to collaborate and gather feedback"],
     dates: "Jan 2022 - Present",
     url: "https://www.blueport.com",
+    linkedin: "https://www.linkedin.com/company/blueport-commerce/",
+    backgroundImage: "LINK",
   },
   {
     position: "ERP Project Intern",
@@ -14,14 +16,18 @@ var experiences = [
     description: ["Conducted manufacturing time studies to improve accuracy of estimates in ERP system", "Organized and distributed drawings from the engineering team to those manufacturing the equipment"],
     dates: "Jun. 2021 - Aug. 2021",
     url: "https://www.wasserstrom.com/restaurant-supplies-equipment/n-wasserstrom-and-sons",
+    linkedin: "https://www.linkedin.com/company/n-wasserstrom-sons/",
+    backgroundImage: "LINK",
   },
   {
     position: "UC & Microsoft Teams Calls Assistant",
     company: "Northeastern University ITS",
     location: "Boston, MA",
-    description: ["Installed hundreds of touchscreen Microsoft Teams enabled VoIP phones across Northeastern’s Boston campus", "Created detailed video guides on how to operate the phone devices for university staff", "Removed old landlines and cleaned up associated wire closets using telecommunication equipment"],
+    description: ["Installed hundreds of touchscreen Microsoft Teams enabled VoIP phones across Northeastern's Boston campus", "Created detailed video guides on how to operate the phone devices for university staff", "Removed old landlines and cleaned up associated wire closets using telecommunication equipment"],
     dates: "Jan. 2021 - May 2021",
     url: "https://its.northeastern.edu",
+    linkedin: "https://www.linkedin.com/company/northeastern-university-information-technology-services/",
+    backgroundImage: "LINK",
   }
 ];
 
