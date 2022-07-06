1aeb6067795272e4538a1e3920bd4f12469b7542
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-07-06 16:05:52 -0400

    Attempting to add location state

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,30 @@ import "./App.css";
 import Homepage from "./components/homepage/Homepage";
 import ExperienceModal from "./components/experience-section/experience-modal/ExperienceModal";
 import Error from "./components/error/Error";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+// Location causing page to render blank
 
 function App() {
+  let location = useLocation();
+  let state = location.state as { backgroundLocation?: Location };
   return (
     <>
       <Router>
-        <Routes>
+        <Routes location={state?.backgroundLocation || location}>
           <Route path="/" element={<Homepage />} />
-          <Route path="/experience/:id" element={<ExperienceModal />} />
           <Route path="*" element={<Error />} />
         </Routes>
+        {state?.backgroundLocation && (
+          <Routes>
+            <Route path="/experience/:id" element={<ExperienceModal />} />
+          </Routes>
+        )}
       </Router>
     </>
   );
