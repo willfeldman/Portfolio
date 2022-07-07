fbf18b1bcd30cea219936854838bf145ed95f880
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-07-06 20:11:12 -0400

    Fixed modal, works with routing and useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,16 @@ import Homepage from "./components/homepage/Homepage";
 import ExperienceModal from "./components/experience-section/experience-modal/ExperienceModal";
 import Error from "./components/error/Error";
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
 } from "react-router-dom";
-
-// Location causing page to render blank
 
 function App() {
   let location = useLocation();
   let state = location.state as { backgroundLocation?: Location };
   return (
     <>
-      <Router>
         <Routes location={state?.backgroundLocation || location}>
           <Route path="/" element={<Homepage />} />
           <Route path="*" element={<Error />} />
@@ -27,7 +23,6 @@ function App() {
             <Route path="/experience/:id" element={<ExperienceModal />} />
           </Routes>
         )}
-      </Router>
     </>
   );
 }
