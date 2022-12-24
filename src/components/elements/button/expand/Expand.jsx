bb834667605f5bc3f8f4774150cb972691f772e8
willfeldman <13539982+willfeldman@users.noreply.github.com>
2022-12-24 17:49:51 -0500

    Custom coloring for expand (icon and background)

--- src/components/elements/button/expand/Expand.jsx
+++ src/components/elements/button/expand/Expand.jsx
@@ -2,7 +2,6 @@ import { CgArrowsExpandRight } from "react-icons/cg";
 import "./Expand.scss";
 
 export default function Expand(props) {
-
   // adds the box shadow if requested in data
   function addBoxShadow() {
     if (props.boxShadow) {
@@ -10,11 +9,35 @@ export default function Expand(props) {
     }
   }
 
+  // sets the default values
+  let iconColor = "black";
+  let backgroundColor = "white";
+
+  if (props.customColors) {
+    // updates iconColor in object if given
+    if (props.customColors.iconColor) {
+      iconColor = props.customColors.iconColor;
+    }
+
+    // updates backgroundColor in object if given
+    if (props.customColors.backgroundColor) {
+      backgroundColor = props.customColors.backgroundColor;
+    }
+  }
+
+  // sets the styling object for the button
+  const style = {
+    width: props.size,
+    height: props.size,
+    backgroundColor: backgroundColor,
+    color: iconColor
+  };
+
   return (
     <div className={`expand ${addBoxShadow()}`}>
       <button
         onClick={props.onClick}
-        style={{ width: props.size, height: props.size }}
+        style={style}
         aria-label="Expand"
       >
         <CgArrowsExpandRight />
--- src/components/elements/card/Card.jsx
+++ src/components/elements/card/Card.jsx
@@ -9,12 +9,33 @@ export default function Card(props) {
     }
   }
 
+  function renderExpandStyling() {
+    // has the expand button styling match card colors
+    if (props.expandable !== false) {
+      // only works if the expand button is requested
+
+      let styling = {};
+
+      // updates iconColor to match card background color if given
+      if (props.backgroundColor) {
+        styling.iconColor = props.backgroundColor;
+      }
+
+      // updates backgroundColor to match card text color if given
+      if (props.textColor) {
+        styling.backgroundColor = props.textColor;
+      }
+
+      return styling;
+    }
+  }
+
   function renderExpandButton() {
     // renders the expand button if it requested in the data
     if (props.expandable !== false) {
       return (
         <div className="expandCardButton">
-          <Expand size="30px" />
+          <Expand size="30px" customColors={renderExpandStyling()} />
         </div>
       );
     }
