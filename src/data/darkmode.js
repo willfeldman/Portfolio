347305e62422c9c1adf629e65c0972debbba99a1
willfeldman <13539982+willfeldman@users.noreply.github.com>
2023-01-08 00:36:46 -0500

    Changes to make iOS status bar color match theme

--- src/data/darkmode.js
+++ src/data/darkmode.js
@@ -4,4 +4,7 @@ const mode = (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night
 // changes the background color of 'html' element based on mode
 document.body.style.backgroundColor = (mode === 'night' ? '#191919' : '#f5f5f5');
 
+// change ios status bar color depending on mode
+document.querySelector('meta[name="theme-color"]').setAttribute('content', (mode === 'night' ? '#191919' : '#f5f5f5'));
+
 export { mode };
