0e010c1595f1e991c9355360ab8cf47fd7cefee4
Will Feldman <13539982+willfeldman@users.noreply.github.com>
2023-12-26 21:22:14 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,56 @@ import Navigation from "./components/navigation/Navigation";
 
 function App() {  
   let location = useLocation();
+  let state = location.state as { backgroundLocation?: Location };
   
   return (
     <div className={`app ${mode}-mode`}>
-      <Routes location={location}>
+      <Routes location={state?.backgroundLocation || location}>
         <Route path="/" element={<Homepage />} />
         <Route path="/index.html" element={<Homepage />} />
-        <Route path="/experience/:id" element={<Modal><ExperienceView /></Modal>} />
+        <Route path="/experience/:id" element={<Navigation><ExperienceView /></Navigation>} />
         <Route path="/project/:id" element={<Navigation><ProjectView /></Navigation>} />
         <Route path="/organization/:id" element={<Navigation><OrganizationView /></Navigation>} />
         <Route path="/award/:id" element={<Navigation><AwardView /></Navigation>} />
         <Route path="/image" element={<ImageView />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route
+            path="/experience/:id"
+            element={
+              <Modal>
+                <ExperienceView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/project/:id"
+            element={
+              <Modal>
+                <ProjectView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/organization/:id"
+            element={
+              <Modal>
+                <OrganizationView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/award/:id"
+            element={
+              <Modal>
+                <AwardView />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 }
