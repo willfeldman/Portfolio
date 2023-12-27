70676f4ed7ee52eedb72d896d1d7befd02b6e156
Will Feldman <13539982+willfeldman@users.noreply.github.com>
2023-12-26 21:41:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route, useLocation } from "react-router-dom"; // Import HashRouter
+import { Routes, Route, useLocation } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
 import ExperienceView from "./components/experience-section/experience-view/ExperienceView";
 import ProjectView from "./components/project-section/project-view/ProjectView";
@@ -18,54 +18,52 @@ function App() {
   
   return (
     <div className={`app ${mode}-mode`}>
-      <HashRouter> {/* Use HashRouter here */}
-        <Routes location={state?.backgroundLocation || location}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/index.html" element={<Homepage />} />
-          <Route path="/experience/:id" element={<Navigation><ExperienceView /></Navigation>} />
-          <Route path="/project/:id" element={<Navigation><ProjectView /></Navigation>} />
-          <Route path="/organization/:id" element={<Navigation><OrganizationView /></Navigation>} />
-          <Route path="/award/:id" element={<Navigation><AwardView /></Navigation>} />
-          <Route path="/image" element={<ImageView />} />
-          <Route path="*" element={<Error />} />
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/index.html" element={<Homepage />} />
+        <Route path="/experience/:id" element={<Navigation><ExperienceView /></Navigation>} />
+        <Route path="/project/:id" element={<Navigation><ProjectView /></Navigation>} />
+        <Route path="/organization/:id" element={<Navigation><OrganizationView /></Navigation>} />
+        <Route path="/award/:id" element={<Navigation><AwardView /></Navigation>} />
+        <Route path="/image" element={<ImageView />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route
+            path="/experience/:id"
+            element={
+              <Modal>
+                <ExperienceView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/project/:id"
+            element={
+              <Modal>
+                <ProjectView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/organization/:id"
+            element={
+              <Modal>
+                <OrganizationView />
+              </Modal>
+            }
+          />
+          <Route
+            path="/award/:id"
+            element={
+              <Modal>
+                <AwardView />
+              </Modal>
+            }
+          />
         </Routes>
-        {state?.backgroundLocation && (
-          <Routes>
-            <Route
-              path="/experience/:id"
-              element={
-                <Modal>
-                  <ExperienceView />
-                </Modal>
-              }
-            />
-            <Route
-              path="/project/:id"
-              element={
-                <Modal>
-                  <ProjectView />
-                </Modal>
-              }
-            />
-            <Route
-              path="/organization/:id"
-              element={
-                <Modal>
-                  <OrganizationView />
-                </Modal>
-              }
-            />
-            <Route
-              path="/award/:id"
-              element={
-                <Modal>
-                  <AwardView />
-                </Modal>
-              }
-            />
-          </Routes>
-        )}
-      </HashRouter> {/* Close HashRouter here */}
+      )}
     </div>
   );
 }
